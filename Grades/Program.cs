5f6015c35e631b1543ff4707790d6d7c50c41bab
Timmithy Young <tim.young.85@gmail.com>
2017-01-27 13:32:28 -0500

    Chapter4_Lesson8
    Assemblies

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace Grades
 {
@@ -22,6 +23,9 @@ namespace Grades
 
         static void Main(string[] args)
         {
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello, World");
             Arrays();
             //Immutable();
             //PassByValueAndRef();
